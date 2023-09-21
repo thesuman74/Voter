676e6368e9b4429ec9b92036549ea95597ecdfb9
Suman Adhikari <58351346+thesuman74@users.noreply.github.com>
2023-09-21 10:39:18 +0545

    php api files inside of xampp/https

---
+++ practice_api/update_data.php
@@ -0,0 +1,35 @@
+<?php
+
+include("dbconnection.php");
+$con = dbconnection();
+
+
+if (isset($_POST["name"])) {
+    $name = $_POST["name"];
+} else return;
+
+if (isset($_POST["email"])) {
+    $email = $_POST["email"];
+} else return;
+
+if (isset($_POST["password"])) {
+    $password = $_POST["password"];
+} else return;
+
+
+$query = "UPDATE `user_table` 
+SET `uname`='$name', `upassword`='$password'
+WHERE `uemail`='$email'";
+
+
+$exe = mysqli_query($con, $query);
+
+
+$arr = [];
+if ($exe) {
+    $arr["success"] = "true";
+} else {
+    $arr["success"] = "false";
+}
+
+print(json_encode($arr));
